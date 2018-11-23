44b4ca77d6b5a12f2be7f1afaf93a15e18754125
Macbook <lei.ctzhu@gmail.com>
2018-11-22 19:11:18 -0500

    Squashed 'facilityLocation/' content from commit b5c1e40
    git-subtree-dir: facilityLocation
    git-subtree-split: b5c1e4075c13c8d48220e0603fbc6e9c58603063

---
+++ README.md
@@ -0,0 +1,18 @@
+# Facility Location
+An example of using Gurobi to solve a facility location problem
+
+![](screenshot.png?raw=true)
+
+# Running the example
+
+1. Start Python's webserver from the command line
+    ```
+    make
+    ```
+
+2. Point your browser at http://localhost:8000
+
+3. Click on the screen to add potential facility locations.
+
+4. Click "Compute" to locate the facilities.
+
---
+++ facility.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+from gurobipy import *
+import math
+import StringIO
+
+clients = [[100,200], [150,250], [650, 200], [50, 300]]
+
+facilities = []; charge = []
+
+for i in range(10):
+    for j in range(10):
+        facilities.append([i*70, j*50])
+        charge.append(1)
+
+def mycallback(model, where):
+    if where == GRB.callback.MESSAGE:
+        print >>model.__output, model.cbGet(GRB.callback.MSG_STRING),
+
+def distance(a,b):
+    dx = a[0] - b[0]
+    dy = a[1] - b[1]
+    return math.sqrt(dx*dx + dy*dy)
+
+def optimize(clients, facilities, charge, output=False):
+    numFacilities = len(facilities)
+
+    numClients = len(clients)
+
+    m = Model()
+
+    if not output:
+        m.params.OutputFlag = 0
+
+    m.setParam('TimeLimit', 10)
+
+    # Add variables
+    x = {}
+    y = {}
+    d = {} # Distance matrix (not a variable)
+
+    for j in range(numFacilities):
+        x[j] = m.addVar(vtype=GRB.BINARY, name="x%d" % j)
+
+    for i in range(numClients):
+        for j in range(numFacilities):
+            y[(i,j)] = m.addVar(lb=0, vtype=GRB.CONTINUOUS, name="t%d,%d" % (i,j))
+            d[(i,j)] = distance(clients[i], facilities[j])
+
+    m.update()
+
+    # Add constraints
+    for i in range(numClients):
+        for j in range(numFacilities):
+            m.addConstr(y[(i,j)] <= x[j])
+
+    for i in range(numClients):
+        m.addConstr(quicksum(y[(i,j)] for j in range(numFacilities)) == 1)
+
+    m.setObjective( quicksum( charge[j]*x[j] + quicksum(d[(i,j)]*y[(i,j)] for i in range(numClients))
+                             for j in range(numFacilities) ), GRB.MINIMIZE)
+
+    output = StringIO.StringIO()
+    m.__output = output
+
+    m.optimize(mycallback)
+
+    if (m.status != 2):
+        return ["error"]
+
+    solution1 = []
+    solution2 = []
+
+    for j in range(numFacilities):
+        if (x[j].X > .5):
+            solution1.append(j)
+
+    for i in range(numClients):
+        for j in range(numFacilities):
+            if (y[(i,j)].X > .5):
+                solution2.append([i,j])
+
+    return [solution1, solution2, output.getvalue()]
+
+def handleoptimize(jsdict):
+    if 'clients' in jsdict and 'facilities' in jsdict and 'charge' in jsdict:
+        solution = optimize(jsdict['clients'], jsdict['facilities'], jsdict['charge'])
+        return {'solution': solution }
+
+if __name__ == '__main__':
+    import json
+    jsdict = json.load(sys.stdin)
+    jsdict = handleoptimize(jsdict)
+    print 'Content-Type: application/json\n\n'
+    print json.dumps(jsdict)
